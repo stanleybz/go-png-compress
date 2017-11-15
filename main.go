a2413bf7adbcbdb64febe39188eaeefbc4145533
Stanley Cheung <>
2017-11-15 19:26:47 +0800

    Add resize option

--- main.go
+++ main.go
@@ -1,26 +1,41 @@
 package main
 
 import (
+	"github.com/nfnt/resize"
 	"image"
   "image/jpeg"
 	"os"
   "flag"
-	"fmt"
+	_"fmt"
 )
 
 func main() {
 
-  inputFile := os.Args[2]
-  outputFile := os.Args[3]
-  quality := flag.Int("q", 50, "an int")
-	flag.Parse()
-	fmt.Print(*quality)
-  fImg1, _ := os.Open(inputFile)
-  defer fImg1.Close()
-  img1, _, _ := image.Decode(fImg1)
+	// Get input file
+  inputFile := os.Args[3]
 
+	// Set output file
+  outputFile := os.Args[4]
+
+	// Check quality
+	quality := flag.Int("q", 50, "an int")
+	width := flag.Int("w", 400, "an int")
+	flag.Parse()
+
+	// Debug log
+	// fmt.Print(inputFile, outputFile, *quality, *width)
+
+	// Open input file and decode
+	fImg1, _ := os.Open(inputFile)
+  defer fImg1.Close()
+	img1, _, _ := image.Decode(fImg1)
+
+	// Create output file
   toimg, _ := os.Create(outputFile)
   defer toimg.Close()
 
-  jpeg.Encode(toimg, img1, &jpeg.Options{*quality})
+	img2 := resize.Resize(uint(*width), 0, img1, resize.Lanczos3)
+
+	// Encode
+  jpeg.Encode(toimg, img2, &jpeg.Options{*quality})
 }
