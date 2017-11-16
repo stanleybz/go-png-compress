ef7c701bd7ba6ee9cd2938bc369296a92adcb1d7
Stanley Cheung <hi@stanley.bz>
2017-11-16 10:32:06 +0800

    Minor update, not working now

--- main.go
+++ main.go
@@ -1,41 +1,41 @@
 package main
 
 import (
-	"github.com/nfnt/resize"
-	"image"
+  "github.com/nfnt/resize"
+  "image"
   "image/jpeg"
-	"os"
+  "os"
   "flag"
 	_"fmt"
 )
 
 func main() {
 
-	// Get input file
+  // Get input file
   inputFile := os.Args[3]
 
-	// Set output file
+  // Set output file
   outputFile := os.Args[4]
 
-	// Check quality
-	quality := flag.Int("q", 50, "an int")
-	width := flag.Int("w", 400, "an int")
-	flag.Parse()
+  // Check quality
+  quality := flag.Int("q", 50, "an int")
+  width := flag.Int("w", 400, "an int")
+  flag.Parse()
 
-	// Debug log
-	// fmt.Print(inputFile, outputFile, *quality, *width)
+  // Debug log
+  // fmt.Print(inputFile, outputFile, *quality, *width)
 
-	// Open input file and decode
-	fImg1, _ := os.Open(inputFile)
+  // Open input file and decode
+  fImg1, _ := os.Open(inputFile)
   defer fImg1.Close()
-	img1, _, _ := image.Decode(fImg1)
+  img1, _, _ := image.Decode(fImg1)
 
-	// Create output file
+  // Create output file
   toimg, _ := os.Create(outputFile)
   defer toimg.Close()
 
-	img2 := resize.Resize(uint(*width), 0, img1, resize.Lanczos3)
+  img2 := resize.Resize(uint(*width), 0, img1, resize.Lanczos3)
 
-	// Encode
+  // Encode
   jpeg.Encode(toimg, img2, &jpeg.Options{*quality})
 }
